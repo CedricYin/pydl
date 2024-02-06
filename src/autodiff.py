3583c025fda66321ea9e564767bbadef29193375
cedric-yin <ysq2023@gmail.com>
2024-02-06 16:06:10 +0800

    step9 done

--- src/autodiff.py
+++ src/autodiff.py
@@ -1,4 +1,4 @@
-"""实现自动微分"""
+"""基于define-by-run（动态计算图），实现自动微分"""
 
 import numpy as np
 
@@ -6,19 +6,39 @@ import numpy as np
 class Variable:
     """变量类，对实际数据进行封装"""
     def __init__(self, data):
+        # 只支持numpy.ndarray类型的数据
+        if data is not None and not isinstance(data, np.ndarray):
+            raise TypeError(f"{type(data)} is not supported")
+
         self.data = data
         self.grad = None
         self.creator = None  # 创造该变量的函数
     
     def set_creator(self, func):
         self.creator = func
+
+    # 递归方式实现反向传播  
+    # def backward(self):
+    #     f = self.creator
+    #     if f is not None:
+    #         x = f.input
+    #         x.grad = f.backward(self.grad)
+    #         x.backward()  # 递归地反向传播到最左边的参数（最左边的参数的creator为None
     
+    # 循环方式实现反向传播
     def backward(self):
-        f = self.creator
-        if f is not None:
-            x = f.input
-            x.grad = f.backward(self.grad)
-            x.backward()  # 递归地反向传播到最左边的参数（最左边的参数的creator为None
+        # 最终的输出对自己的导数是1
+        if self.grad is None:
+            self.grad = np.ones_like(self.data)
+
+        funcs = [self.creator]
+        while funcs:
+            f = funcs.pop()
+            x, y = f.input, f.output  # 获取中间结果
+            x.grad = f.backward(y.grad)
+
+            if x.creator is not None:
+                funcs.append(x.creator)
 
 
 class Function:
@@ -26,7 +46,7 @@ class Function:
     def __call__(self, input):
         x = input.data
         y = self.forward(x)
-        output = Variable(y)
+        output = Variable(as_array(y))
         output.set_creator(self)  # define-by-run, 在计算之间建立连接
         # 保存中间数据
         self.input = input  # 保存输入变量（反向传播所需要的中间值，这里是输入）
@@ -51,6 +71,11 @@ class Square(Function):
         x = self.input.data
         gx = 2 * x * gy
         return gx
+
+
+def square(x):
+    """暴露给用户的接口：计算平方"""
+    return Square()(x)
     
 
 class Exp(Function):
@@ -62,6 +87,19 @@ class Exp(Function):
         x = self.input.data
         gx = np.exp(x) * gy
         return gx
+
+
+def exp(x):
+    """暴露给用户的接口：计算exp"""
+    return Exp()(x)
+
+
+def as_array(x):
+    """将x转化为ndarray：
+    因为0维的ndarray计算后会得到标量类型，这里为了避免TypeError，实现了该函数"""
+    if np.isscalar(x):
+        return np.array(x)
+    return x
 
 
 def numerical_dirr(f, x, eps=1e-4):
@@ -82,6 +120,6 @@ def demo_f(x):
 
 
 if __name__ == "__main__":
-    x = Variable(np.array(0.5))
-    dy = numerical_dirr(demo_f, x)
-    print(dy)
+    x = np.array(2)
+    print(x ** 2)
+    print(type(x ** 2))
--- test/test_autodiff.py
+++ test/test_autodiff.py
@@ -1,21 +1,18 @@
-# 如果要运行该文件，需要先export PYTHONPATH=${PYTHONPATH}:/path/to/pydl，避免src模块找不到
+# 如果要运行该文件（不使用pytest），
+# 需要先export PYTHONPATH=${PYTHONPATH}:/path/to/pydl，避免src模块找不到。
 
 from src.autodiff import *
 import numpy as np
 
 def test_backward():
-    A = Square()
-    B = Exp()
-    C = Square()
     x = Variable(np.array(0.5))
 
-    # 反向传播前需要先进行前向传播，并存储中间值，这里是输出
-    a = A(x)
-    b = B(a)
-    y = C(b)
+    # 反向传播前需要先进行前向传播
+    a = square(x)
+    b = exp(a)
+    y = square(b)
 
     # 反向传播
-    y.grad = np.array(1.0)
     y.backward()
-    
+
     assert x.grad == 3.297442541400256
